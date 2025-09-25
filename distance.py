4400e3525abeb6f2afb9b3dc0a7703a0acb55358
naefdominik <naef.dominik@hotmail.com>
2025-09-25 16:10:10 +0200

    translate distance array to single value

--- distance.py
+++ distance.py
@@ -4,7 +4,7 @@ from math import sqrt
 import time
  
 def runExample():
-    print("\nQwiic VL53LCX Example 1 - DistanceArray\n")
+    print("\nQwiic VL53LCX Distance Test\n")
  
     myVL53L5CX = qwiic_vl53l5cx.QwiicVL53L5CX() 
  
@@ -19,19 +19,46 @@ def runExample():
  
     myVL53L5CX.set_resolution(8*8) # enable all 64 pads
     image_resolution = myVL53L5CX.get_resolution()  # Query sensor for current resolution - either 4x4 or 8x8
- 
-    image_width = int(sqrt(image_resolution)) # Calculate printing width
+    #image_width = int(sqrt(image_resolution)) # Calculate printing width
+
     myVL53L5CX.start_ranging()
  
     while True:
         if myVL53L5CX.check_data_ready():
             measurement_data = myVL53L5CX.get_ranging_data()
-            for y in range (0, (image_width * (image_width - 1) )+ 1, image_width):
-                for x in range (image_width - 1, -1, -1):
-                    print("\t", end="")
-                    print(measurement_data.distance_mm[x + y], end = "")
-                print("\n")
-            print("\n")
+
+            # extract all distances into a list
+            distances = [measurement_data.distance_mm[i] for i in range(image_resolution)]
+
+            # test 1: minimum distance (closest object)
+            distance_value = min(distances)
+
+            # test 2: average distance
+            #distance_value = sum(distances) / len(distances)
+
+            # test 3: center pixel only (row 4, col 4 in 8x8 mode)
+            #center_index = (image_width // 2) * image_width + (image_width // 2)
+            # distance_value = measurement_data.distance_mm[center_index]
+
+            #for y in range (0, (image_width * (image_width - 1) )+ 1, image_width):
+            #    for x in range (image_width - 1, -1, -1):
+            #        print("\t", end="")
+            #        print(measurement_data.distance_mm[x + y], end = "")
+            #    print("\n")
+            #print("\n")
+
+            print("Distance (mm): %d" % distance_value)
+
+            # TODO: feedback logic
+            if distance_value < 300:  # closer than 30 cm
+                print("Object is very close!")
+                pass
+            elif distance_value < 1000:  # closer than 1 m
+                print("Object is at a moderate distance.")
+                pass
+            else:
+                print("Object is far away.")
+                pass
         
         time.sleep(0.005)
     
